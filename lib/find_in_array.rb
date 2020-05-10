bf34466d542d201282e10c014d05b44d4cdd6395
Scott Espinosa <scottjames.espinosa@gmail.com>
2020-05-10 04:11:21 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,10 +1,10 @@
 def find_element_index(array, value_to_find)
  count = 0 
  while count < array.length do
-   if array[count] = value_to_find
-     p count
+   if array[count] == value_to_find
+     puts count
    end
      count += 1
  end 
- p nil
+ puts nil
 end
